a6fae4ff3b52c941823696a7b19a122b49e69094
Aintripin <yelisey_toyota@mail.ru>
2022-03-21 17:42:15 +0300

    added images and buttons

--- tesla-clone/src/components/Section.js
+++ tesla-clone/src/components/Section.js
@@ -4,7 +4,18 @@ import styled from "styled-components"
 function Section() {
     return (
         <Wrap>
-            Section
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
         </Wrap>
     )
 }
@@ -14,5 +25,39 @@ export default Section
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background: orange;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/model-s.jpg');
+    display: flex;
+    flex-direction: column;             // puttin 'em top to bottom
+    justify-content: space-between;     // vertical
+    align-items: center;                // horizontal
+`
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;              // centering
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+`
+
+const RightButton = styled(LeftButton)`         // inherting all the CSS of the LeftButtin
+
 `
